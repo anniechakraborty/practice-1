8fe65a58db964b5b3f06aae4c6f0151d0f313288
Annie Chakraborty <the.demented.genius29@gmail.com>
2020-08-02 13:33:50 +0530

    pressing the button changes the text

--- lib/main.dart
+++ lib/main.dart
@@ -3,60 +3,64 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Practice 1',
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _HomeState extends State<Home> {
+  List _data = [
+    'Hello',
+    'Hi',
+    'I am Annie',
+    'Lorem Ipsum',
+    'Jenny',
+    'New Text'
+  ];
+  int _index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        title: Text(
+          'HOME',
         ),
+        backgroundColor: Colors.teal.shade800,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      backgroundColor: Colors.teal.shade900,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Text(_data[_index]),
+            flex: 5,
+          ),
+          Expanded(
+            flex: 2,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  if (_index < _data.length) {
+                    _index++;
+                  } else {
+                    _index = 0;
+                  }
+                });
+              },
+              child: Text('Change Text'),
+            ),
+          )
+        ],
       ),
     );
   }
